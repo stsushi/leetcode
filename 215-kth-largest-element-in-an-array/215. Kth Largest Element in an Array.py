938b2c644c84ddd7570df82d76b67f8e636ea692
Andy Lo <and15989@gmail.com>
2024-03-10 22:35:22 -0700

    Runtime: 441 ms (88.37%), Memory: 30.8 MB (14.17%) - Gitcode

---
+++ 215-kth-largest-element-in-an-array/215. Kth Largest Element in an Array.py
@@ -0,0 +1,25 @@
+from random import choice
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # naive approach sort it and return
+        # better approach bucket the elements n time to put in buckets m runtime to check buckets and return starting from max->min
+        # best approach quick select n runtime 
+        def quickselect(nums, k):
+            pivot = choice(nums)
+            smaller = []
+            bigger = []
+            same = []
+            for num in nums:
+                if num < pivot:
+                    smaller.append(num)
+                elif num > pivot:
+                    bigger.append(num)
+                else:
+                    same.append(num)
+            if len(bigger) >= k:
+                return quickselect(bigger,k)
+            elif len(same)+len(bigger)< k:
+                return quickselect(smaller,k-len(bigger)-len(same))
+            else:
+                return pivot
+        return quickselect(nums,k)
