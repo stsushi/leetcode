2947e2d952882f97799038ba6bc4f255ed681ab7
Andy Lo <and15989@gmail.com>
2024-03-10 22:05:49 -0700

    Runtime: 123 ms (96.24%), Memory: 23.8 MB (5.17%) - Gitcode

--- 528-random-pick-with-weight/528. Random Pick with Weight.py
+++ 528-random-pick-with-weight/528. Random Pick with Weight.py
@@ -1,46 +1,42 @@
 class Solution:
-    # possible solutions prefix sum + binary search
-    # init -> prefix sum o(n) runtime
-    # pickindex -> binary search log(n) runtime
 
-    # alias method pack everything into dict like struct with max len 2
-    # init -> n runtime at most iter over each elem once
-    # pick -> 1 runtime takes at most 2 box check to pick 
-
-    def __init__(self, w: List[int]): 
+    def __init__(self, w: List[int]):
         ep =  10e-5 #variable due to rounding errors on floats
-        self.n = len(w) # len of original list and actually also the len of the boxes so not technically needed
-        weight_sum = sum(w) # the total sum of all elements (only needed to get the elements as percents)
-        self.box_size = 1/self.n # the size of each index
-        w = [x/weight_sum for x in w] #each elem is now what percent of the total it is
+        sum_w = sum(w)
+        n = len(w)
+        self.n = len(w) # too lazy to refactor... realized i need this in pickindex
+        box_size = 1/n
+        self.box_size = 1/n # too lazy to refactor... realized i need this in pickindex
+        w = [x/sum_w for x in w] #converts them into percent of whole
 
-        self.boxes = [] # holds the probabilities
-        big = {i:val for (i, val) in enumerate(w) if  val >= self.box_size} # probabilities greater than or equal to the right size
-        small = {i:val for (i, val) in enumerate(w) if  val < self.box_size} # boxes smaller than the right size
-        #algo works like this bigger elements will be used to perfectly fill smaller ones
-        #this means that every elem will at most have 2 options in it
-        # big ones might become small ones in this process and get added to the small stack
+        small = { i:x for i,x in enumerate(w) if x < box_size}
+        big = { i:x for i,x in enumerate(w) if x >= box_size}
+        
+        self.boxes = []
+
         while small and big:
-            j, size = small.popitem() # 
-            i, big_size = big.popitem() # can replace with some iter thing but i dont want to get into syntax hell
-            
-            self.boxes.append((j,i,size))
+            small_index, small_size = small.popitem()
+            big_index, big_size = big.popitem()
 
-            big_size = big_size-(self.box_size-size) # new size of big box
-            if big_size <  self.box_size-ep: #constant is due to rounding errors on floats
-                small[i] = big_size #big box now small
+            self.boxes.append((small_index,big_index,small_size))
+
+            new_big_size = big_size +small_size - box_size
+            if new_big_size >= box_size:
+                big[big_index] = new_big_size
             else:
-                big[i] = big_size #big box still big 
-        self.boxes.extend((key,key,self.box_size) for key in big) # add the perfect sized ones
+                small[big_index] = new_big_size
+        self.boxes.extend((key,key, box_size) for key in big)
+        self.boxes.extend((key, key, box_size) for key in small) #rounding error
+
+
 
 
     def pickIndex(self) -> int:
-        r = random.random()*self.n # range 0->2 
-        i, line = int(r), random.random()* self.box_size #one random var is for which box we are in, the second random var is for which elem of the box we are in... Could also be replaced by using the remainder of the first var and doing some fancy math lol
-        print(self.boxes[i][0] if self.boxes[i][2]> line else self.boxes[i][1])
-        return self.boxes[i][0] if self.boxes[i][2]> line else self.boxes[i][1]
+        i = int(random.random()*self.n)
+        j = random.random()*self.box_size
+        pick = self.boxes[i]
+        return pick[0] if j < pick[2] else pick[1]
 
-        
 
 
 # Your Solution object will be instantiated and called as such:
