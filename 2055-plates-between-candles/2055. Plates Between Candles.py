8af8731d8af9a3eff8fee40f9dd10316e7b658d4
Andy Lo <and15989@gmail.com>
2024-03-11 03:31:31 -0700

    Runtime: 1491 ms (67.00%), Memory: 56.8 MB (10.82%) - Gitcode

--- 2055-plates-between-candles/2055. Plates Between Candles.py
+++ 2055-plates-between-candles/2055. Plates Between Candles.py
@@ -1,52 +1,31 @@
 class Solution:
     def platesBetweenCandles(self, s: str, qs: List[List[int]]) -> List[int]:
         n=len(s)
-        prefcandle=[-1]*n #this stores the position of closest candle from current towards left
-        suffcandle=[0]*n #this stores the position of closest candle from current towards right
+        prefix = [None]*n
+        suffix = [None]*n
+        candles = [0]*n
+
+        count = 0
+        prefix_i = None
+        for i, elem in enumerate(s):
+            if elem == '|':
+                prefix_i = i
+            else:
+                count+=1
+            candles[i] = count
+            prefix[i] = prefix_i
         
-        pref=[0]*n #stores the number of plates  till ith position from 0 - for i = 0 -> n 
-        
-        ind=-1
-        c=0
-        #The following method calculates number of plates(*) till ith position from 0 - for i = 0 -> n 
-        for i in range(n):
-            if ind!=-1 and s[i]=='*':
-                c+=1
-            elif s[i]=='|':
-                ind=i
-            pref[i]=c
-              
-        #this method calculates the left nearest candle to a point
-        #intial is -1 as to left of leftmost element no candle can be present
-        ind =-1
-        for i in range(n):
+        suffix_i = None
+        for i in range(n-1,-1,-1):
             if s[i] == '|':
-                ind=i
-            prefcandle[i]=ind
-            
-        #this method calculates the right nearest candle to a point
-        #intial is infinity as to right of rightmost element no candle can be present
-        ind = float('inf')       
-        for i in range(n-1, -1, -1):
-            if s[i]=='|':
-                ind=i
-            suffcandle[i]=ind
-
-        #m = no of queries
-        m=len(qs)
-        ans=[0]*m
-
-        for i in range(m):
-            c=0
-            l=qs[i][0]
-            r=qs[i][1]
-            
-            #check if left nearest candle of right boundary is after left boundary
-            #check if right nearest candle of left boundary is before right boundary
-            # to summarise - here we find if there is a pair of candle present within the given range or not
-            if prefcandle[r]<l or suffcandle[l]>r:
-                continue
-            
-            #desired answer is no of pplates(*) only inside 2 candles (|) inside the given boundary area
-            ans[i]=pref[prefcandle[r]]-pref[suffcandle[l]]
+                suffix_i = i
+            suffix[i] = suffix_i
+        ans = []
+        print(candles,prefix, suffix)
+        for left,right in qs:
+            if prefix[right] is not None and suffix[left] is not None and prefix[right] > suffix[left]:
+                ans.append(candles[prefix[right]]-candles[suffix[left]])
+            else:
+                ans.append(0)
+        
         return ans
