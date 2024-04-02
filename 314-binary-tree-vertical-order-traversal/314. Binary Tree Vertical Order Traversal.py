11385753a0659549c92febc4f3260b99354bad87
Andy Lo <and15989@gmail.com>
2024-04-02 11:58:18 -0700

    Runtime: 35 ms (75.05%), Memory: 16.5 MB (92.93%) - Gitcode

---
+++ 314-binary-tree-vertical-order-traversal/314. Binary Tree Vertical Order Traversal.py
@@ -0,0 +1,33 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+
+        record = dict()
+        queue = deque([(root, 0)])
+
+        while queue:
+            curr, currpos = queue.popleft()
+
+            record[currpos] = record.get(currpos, []) + [curr.val]
+
+            if curr.left:
+                queue.append((curr.left, currpos - 1))
+            if curr.right:
+                queue.append((curr.right, currpos + 1))
+            
+
+        offset = -1 * min(record.keys())
+        print(record, offset)
+
+        return [record[i - offset] for i in range(len(record))]
+
+
+
+       
