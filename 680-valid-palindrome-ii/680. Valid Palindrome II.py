e2840e6114f51843821a26b2e40e7db8e7e150a1
Andy Lo <and15989@gmail.com>
2024-04-05 01:19:53 -0700

    Runtime: 124 ms (15.15%), Memory: 19.1 MB (7.77%) - Gitcode

--- 680-valid-palindrome-ii/680. Valid Palindrome II.py
+++ 680-valid-palindrome-ii/680. Valid Palindrome II.py
@@ -1,15 +1,17 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
-        def helper(i,j,canSkip=True):
-
-            while(i < j):
-                if s[i]==s[j]:
+        # have to make it recursive to save the state
+        def verify(i,j, can_skip):
+            if i < 0 or j > len(s)-1:
+                return False
+            while i < j:
+                if s[i] != s[j]:
+                    if can_skip:
+                        return verify(i+1,j,False) or verify(i,j-1, False)
+                    else:
+                        return False
+                else:
                     i+=1
                     j-=1
-                elif canSkip:
-                    return helper(i+1,j,False) or  helper(i,j-1,False) 
-                else:
-                    return False
             return True
-        return helper(0,len(s)-1)
+        return verify(0,len(s)-1,True)
