5da7fb99986041e0645043a671810c15ea13dcb1
Andy Lo <and15989@gmail.com>
2024-04-05 01:19:33 -0700

    Runtime: 112 ms (30.57%), Memory: 19 MB (9.56%) - Gitcode

---
+++ 680-valid-palindrome-ii/680. Valid Palindrome II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        def helper(i,j,canSkip=True):
+            while(i < j):
+                if s[i]==s[j]:
+                    i+=1
+                    j-=1
+                elif canSkip:
+                    return helper(i+1,j,False) or  helper(i,j-1,False) 
+                else:
+                    return False
+            return True
+        return helper(0,len(s)-1)
