7e385a9a29f840408f512f2e521e7a45472e2501
Andy Lo <and15989@gmail.com>
2024-04-04 21:26:42 -0700

    Runtime: 36 ms (52.92%), Memory: 16.6 MB (59.47%) - Gitcode

--- 408-valid-word-abbreviation/408. Valid Word Abbreviation.py
+++ 408-valid-word-abbreviation/408. Valid Word Abbreviation.py
@@ -3,16 +3,17 @@ class Solution:
         num = 0
         num_set = set("1234567890")
         ptr = 0
-        for letter in abbr:
-            if letter in num_set:
-                if num == 0 and letter == "0":
+        for elem in abbr:
+            if elem in num_set:
+                if elem == '0' and num == 0:
                     return False
-                num = num*10+int(letter)
+                num = num *10 + int(elem)
             else:
-                ptr+= num
-                num= 0
-                if ptr > len(word)-1 or word[ptr] != letter:
+                ptr +=num
+                num = 0
+                if ptr > len(word)-1 or word[ptr] != elem:
                     return False
                 ptr+=1
-        ptr+= num
-        return False if ptr!=len(word) else True
+
+        ptr +=num
+        return True if ptr == len(word) else False
