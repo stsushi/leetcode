cbdd22bc06a8c3deea93294da4b25d76e161ff04
Andy Lo <and15989@gmail.com>
2024-04-06 01:10:11 -0700

    Runtime: 80 ms (89.18%), Memory: 19.8 MB (59.22%) - Gitcode

--- 227-basic-calculator-ii/227. Basic Calculator II.py
+++ 227-basic-calculator-ii/227. Basic Calculator II.py
@@ -2,27 +2,26 @@ class Solution:
     def calculate(self, s: str) -> int:
         num_set = set('1234567890')
         op_set = set('/*-+')
-        def ans(index):
-            stack, num, op = [], 0, "+"
-            def calculate(num, op):
-                if op == '+': return stack.append(num)
-                if op == '-': return stack.append(-num)
-                if op == "*": return stack.append(stack.pop()* num)
-                if op == '/': return stack.append(int(stack.pop() / num))
-            while(index < len(s)):
-                elem = s[index]
-                if elem in op_set:
-                    calculate(num,op)
-                    num, op = 0, elem
-                if elem in num_set:
-                    num= num*10+int(elem)
-                if elem == ")":
-                    calculate(num,op)
-                    return  sum(stack), index
-                if elem == "(":
-                    num, j = calc(index+1)
-                    index = j
-                index+=1
-            calculate(num,op)
-            return sum(stack)
-        return ans(0)
+        stack = []
+        def evaluate(num, op):
+            if op == "*": return stack.append(stack.pop() * num)
+            if op == "/": return stack.append(int(stack.pop()/num))
+        num, op = 0, "+"
+        for elem in s:
+            if elem in num_set:
+                num = num*10 + int(elem)
+            if elem in op_set:
+                if op == "*" or op == "/":
+                    evaluate(num,op)
+                elif op == "+":
+                    stack.append(num)
+                else:
+                    stack.append(-num)
+                num, op = 0, elem
+        if op == "*" or op == "/":
+            evaluate(num, op)
+        elif op == "+":
+            stack.append(num)
+        else:
+            stack.append(-num)
+        return sum(stack)
