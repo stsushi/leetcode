7da011e04f0a5be13248922cabe19a1add5e5553
Andy Lo <and15989@gmail.com>
2024-04-07 22:28:11 -0700

    Runtime: 106 ms (53.22%), Memory: 19.8 MB (59.24%) - Gitcode

--- 227-basic-calculator-ii/227. Basic Calculator II.py
+++ 227-basic-calculator-ii/227. Basic Calculator II.py
@@ -1,18 +1,22 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack, num, i, op = [], 0, 0, "+"
-        def calculate(num, op):
-            if op == '+': return stack.append(num)
-            if op == '-': return stack.append(-num)
-            if op == "*": return stack.append(stack.pop()* num)
-            if op == '/': return stack.append(int(stack.pop() / num))
         num_set = set('1234567890')
         op_set = set('/*-+')
-        for elem in s:
-            if elem in op_set:
-                calculate(num,op)
-                num, op = 0, elem
-            if elem in num_set:
-                num= num*10+int(elem)
-        calculate(num,op)
-        return sum(stack)
+        def ans(index):
+            stack, num, op = [], 0, "+"
+            def calculate(num, op):
+                if op == '+': return stack.append(num)
+                if op == '-': return stack.append(-num)
+                if op == "*": return stack.append(stack.pop()* num)
+                if op == '/': return stack.append(int(stack.pop() / num))
+            while(index < len(s)):
+                elem = s[index]
+                if elem in op_set:
+                    calculate(num,op)
+                    num, op = 0, elem
+                if elem in num_set:
+                    num= num*10+int(elem)
+                index+=1
+            calculate(num,op)
+            return sum(stack)
+        return ans(0)
