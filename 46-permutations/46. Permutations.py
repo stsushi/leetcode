fec19b88e5cb99473235c8388f26f363770e06ca
Andy Lo <and15989@gmail.com>
2024-05-09 21:55:42 -0700

    Runtime: 46 ms (21.20%), Memory: 16.8 MB (37.55%) - Gitcode

--- 46-permutations/46. Permutations.py
+++ 46-permutations/46. Permutations.py
@@ -1,12 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def recurive(nums,temp,ans): 
-            if not nums:
-                ans.append(temp)
-            for n in range(len(nums)):
-                recurive(nums[0:n]+nums[n+1:],temp+[nums[n]],ans)
-        temp = []
-        recurive(nums,temp,ans)
+        so_far = []
+        seen = set()
+        def backtrack():
+            if len(so_far) == len(nums):
+                ans.append(so_far[:])
+            for elem in nums:
+                if elem in seen:
+                    pass
+                else:
+                    so_far.append(elem)
+                    seen.add(elem)
+                    backtrack()
+                    so_far.pop()
+                    seen.remove(elem)
+        backtrack()
         return ans
-        
