bd7065a3ef6937a00e77a3a19264d0e171781725
Andy Lo <and15989@gmail.com>
2024-05-09 21:51:40 -0700

    Runtime: 46 ms (21.20%), Memory: 16.8 MB (37.55%) - Gitcode

--- 46-permutations/46. Permutations.py
+++ 46-permutations/46. Permutations.py
@@ -1,13 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def get_perm(seq, initial):
-            if len(seq) == 1:
-                yield initial + seq
-            tested = set()
-            for i in range(len(seq)):
-                if seq[i] not in tested:
-                    tested.add(seq[i])
-                    tmp = seq[:]
-                    tmp[i] = tmp[0]
-                    yield from get_perm(tmp[1:], initial + [seq[i]])
-        return get_perm(nums, [])
+        ans = []
+        so_far = []
+        seen = set()
+        def backtrack():
+            if len(so_far) == len(nums):
+                ans.append(so_far[:])
+            for elem in nums:
+                if elem in seen:
+                    pass
+                else:
+                    so_far.append(elem)
+                    seen.add(elem)
+                    backtrack()
+                    so_far.pop()
+                    seen.remove(elem)
+        backtrack()
+        return ans
